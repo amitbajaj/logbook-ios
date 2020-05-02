ac92acb7dc8b180124e4efe590f1640dd0324921
Amit Bajaj <amit@bajajtech.in>
2020-05-02 17:54:40 +0300

    Initial Commit

---
+++ logbook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  logbook
+//
+//  Created by Amit Bajaj on 5/2/20.
+//  Copyright © 2020 Bajaj Tech. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
