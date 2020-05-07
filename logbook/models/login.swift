958efd1683daab19f1246478628e87a4cc5ca828
Amit Bajaj <amit@bajajtech.in>
2020-05-07 10:11:56 +0300

    Initial commit

---
+++ logbook/models/login.swift
@@ -0,0 +1,14 @@
+//
+//  Login.swift
+//  logbook
+//
+//  Created by Amit Bajaj on 5/2/20.
+//  Copyright © 2020 Bajaj Tech. All rights reserved.
+//
+
+import Foundation
+
+struct Login: Codable{
+    let code: String
+    let status: String
+}
